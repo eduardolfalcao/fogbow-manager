b7c87c5a05d32e0ddafc9a3b41fb00ab88864ee5
eduardolfalcao <eduardolfalcao@lsd.ufcg.edu.br>
2017-06-22 14:50:09 -0300

    Fixing csv reader for d, p, k, fairness and satisfaction

--- src/main/java/org/fogbowcloud/manager/experiments/data/CsvReader.java
+++ src/main/java/org/fogbowcloud/manager/experiments/data/CsvReader.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FilenameFilter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -12,133 +13,235 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Properties;
 
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
+
 public class CsvReader {
 	
-//	private int time, demandTot, demandFed, supply;
-//	private int consumingTime, supplyingTime;
-//	
-//	public int getTime() {
-//		return time;
-//	}
-//	public int getDemandTot() {
-//		return demandTot;
-//	}
-//	public int getDemandFed() {
-//		return demandFed;
-//	}
-//	public int getSupply() {
-//		return supply;
-//	}	
-//	public int getConsumingTime() {
-//		return consumingTime;
-//	}
-//	public int getSupplyingTime() {
-//		return supplyingTime;
-//	}
-//	
-//	private String path;
-//	
-//	public CsvReader(String path) {
-//		this.path = path;
-//	}
-//	
-//	private List<PeerState> readFile(String filename){
-//		
-//		List<PeerState> entries = new ArrayList<PeerState>();
-//		
-//		String csvFile = path+filename;
-//        BufferedReader br = null;
-//        String line = "";
-//        String cvsSplitBy = ",";
-//
-//        try {
-//            br = new BufferedReader(new FileReader(csvFile));
-//            br.readLine();	//skip the first
-//            while ((line = br.readLine()) != null) {                
-//                String[] info = line.split(cvsSplitBy);	// use comma as separator             
-//                entries.add(new PeerState(info[0], Integer.parseInt(info[1]), Integer.parseInt(info[2]), Integer.parseInt(info[3]), Integer.parseInt(info[4])));
-//            }
-//        } catch (FileNotFoundException e) {
-//            e.printStackTrace();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        } finally {
-//            if (br != null) {
-//                try {
-//                    br.close();
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//        }
-//		
-//		return entries;
-//	}
-//	
-//	private File[] getListOfFilenames(){
-//		File folder = new File(path);
-//		return folder.listFiles();
-//	}
-//	
-//	private void metricsCalculator(List<PeerState> entries){
-//		PeerState last = null, current = null;
-//		for(int i = 0; i < entries.size()-1; i++){
-//			last = entries.get(i);
-//			current = entries.get(i+1);
-//			
-//			int interval = current.getTime()-last.getTime();
-//			time += interval;
-//			demandTot += interval * last.getDemand();
-//			demandFed += interval * Math.max(0, (last.getDemand()-last.getMaxCapacity()));
-//			supply += interval * last.getSupply();
-//			if(last.getMaxCapacity()-last.getDemand()>=0)
-//				supplyingTime += interval;
-//			else
-//				consumingTime += interval;
-//		}		
-//	}
-//	
-////	private PeerState getNextTime(List<PeerState> entries){		
-////		if(entries.size()>0){
-////			long time = entries.get(0).getTime();
-////			List<PeerState> entriesWithSameTime = new ArrayList<PeerState>();
-////			Iterator<PeerState> it = entries.iterator();
-////			while(it.hasNext()){
-////				PeerState entry = it.next();
-////				if(time == entry.getTime()){
-////					entriesWithSameTime.add(entry);
-////					it.remove();
-////				}
-////			}
-////			return entriesWithSameTime;
-////		}
-////		else return null;		
-////	}
-//	
-//	public static void main(String[] args) throws IOException {
-//		
-//		String managerConfigFilePath = args[0];
-//		File managerConfigFile = new File(managerConfigFilePath);
-//		Properties properties = new Properties();
-//		FileInputStream input = new FileInputStream(managerConfigFile);
-//		properties.load(input);
-//		
-//		CsvReader csv = new CsvReader(properties.getProperty(MonitorPeerState.OUTPUT_FOLDER));		
-//		
-//		for(File f : csv.getListOfFilenames()){
-//			List<PeerState> entries = new ArrayList<PeerState>();
-//			entries.addAll(csv.readFile(f.getName()));
-//			csv.metricsCalculator(entries);
-//		}
-//		System.out.println("d(total)="+csv.getDemandFed()+", s(total)="+csv.getSupply()+", time(total)="+csv.getTime());
-//		System.out.println("*********************************");
-//		System.out.println("k="+((double)csv.getDemandFed())/((double)csv.getSupply()));
-//		System.out.println("dTotal(média)="+((double)csv.getDemandTot())/((double)csv.getConsumingTime()));
-//		System.out.println("consumingProbability="+((double)csv.getConsumingTime())/((double)csv.getTime()));
-//		
-//		
-//		
-//		
-//	}
+	private int time, dTot, dFed, rFed, oFed, sFed;
+	private int consumingTimeFed, consumingTimeTot, supplyingTime;
+	
+	
+	
+	public int getTime() {
+		return time;
+	}
+
+	public void setTime(int time) {
+		this.time = time;
+	}
+
+	public int getdTot() {
+		return dTot;
+	}
+
+	public void setdTot(int dTot) {
+		this.dTot = dTot;
+	}
+
+	public int getdFed() {
+		return dFed;
+	}
+
+	public void setdFed(int dFed) {
+		this.dFed = dFed;
+	}
+	
+	public int getrFed() {
+		return rFed;
+	}
+
+	public void setrFed(int rFed) {
+		this.rFed = rFed;
+	}
+
+
+	public int getoFed() {
+		return oFed;
+	}
+
+	public void setoFed(int oFed) {
+		this.oFed = oFed;
+	}
+
+	public int getsFed() {
+		return sFed;
+	}
+
+	public void setsFed(int sFed) {
+		this.sFed = sFed;
+	}
+
+	public int getConsumingTimeFed() {
+		return consumingTimeFed;
+	}
+
+	public void setConsumingTimeFed(int consumingTimeFed) {
+		this.consumingTimeFed = consumingTimeFed;
+	}
+	
+	public int getConsumingTimeTot() {
+		return consumingTimeTot;
+	}
+
+	public void setConsumingTimeTot(int consumingTimeTot) {
+		this.consumingTimeTot = consumingTimeTot;
+	}
+
+	public int getSupplyingTime() {
+		return supplyingTime;
+	}
+
+	public void setSupplyingTime(int supplyingTime) {
+		this.supplyingTime = supplyingTime;
+	}
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+
+	private String path;
+	
+	public CsvReader(String path) {
+		this.path = path;
+	}
+	
+	private List<PeerState> readFile(String filename){
+		
+		List<PeerState> entries = new ArrayList<PeerState>();
+		
+		String csvFile = path+filename;
+        BufferedReader br = null;
+        String line = "";
+        String cvsSplitBy = ",";
+
+        try {
+            br = new BufferedReader(new FileReader(csvFile));
+            br.readLine();	//skip the first
+            while ((line = br.readLine()) != null) {                
+                String[] info = line.split(cvsSplitBy);	// use comma as separator             
+                entries.add(new PeerState(info[0], Integer.parseInt(info[1]), Integer.parseInt(info[2]), Integer.parseInt(info[3]), Integer.parseInt(info[4]), Integer.parseInt(info[5]), Integer.parseInt(info[6])));
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }		
+		return entries;
+	}
+	
+	private File[] getListOfFilenames(){
+		File folder = new File(path);
+		return folder.listFiles(new FilenameFilter() {
+		    @Override
+		    public boolean accept(File dir, String name) {
+		        return name.endsWith(".csv");
+		    }
+		});
+	}
+	
+	private Results metricsCalculator(List<PeerState> entries){
+		PeerState last = null, current = null;
+		int dFedI = 0, rFedI = 0, sFedI = 0;
+		for(int i = 0; i < entries.size()-1; i++){
+			last = entries.get(i);
+			current = entries.get(i+1);
+			
+			int interval = current.getTime()-last.getTime();
+			
+			//computation of P avg (probability of being consumer)
+			time += interval;
+			if(last.getdFed()>0)
+				consumingTimeFed += interval;
+			
+			//computation of C avg (offer)
+			oFed += interval * last.getoFed();
+			if(last.getoFed()>0)
+				supplyingTime += interval;
+			
+			//computation of D avg (total demand)
+			dTot += interval * last.getdTot();
+			if(last.getdTot()>0)
+				consumingTimeTot += interval;
+			
+			/** Fairness **/
+			//received from federation
+			rFedI += interval * last.getrFed();
+			//donated to federation
+			sFedI += interval * last.getsFed();
+			
+			/** Satisfaction **/
+			//computation of D avg (total demand)
+			dFedI += interval * last.getdFed();
+			
+		}
+		
+		rFed += rFedI;
+		sFed += sFedI;
+		dFed += dFedI;
+		
+		double fairness = -1, satisfaction = -1;
+		if(sFedI>0)
+			fairness = ((double)rFedI/(double)sFedI);
+		if(dFedI>0)
+			satisfaction = ((double)rFedI/(double)dFedI);
+		
+		return new Results(entries.get(0).getId(), fairness, satisfaction);
+		
+	}
+	
+	public static void main(String[] args) throws IOException {
+		
+		String managerConfigFilePath = args[0];
+		File managerConfigFile = new File(managerConfigFilePath);
+		Properties properties = new Properties();
+		FileInputStream input = new FileInputStream(managerConfigFile);
+		properties.load(input);
+		
+		CsvReader csv = new CsvReader(properties.getProperty(MonitorPeerState.OUTPUT_FOLDER));
+		
+		List<Results> results = new ArrayList<Results>();
+		
+		for(File f : csv.getListOfFilenames()){
+			List<PeerState> entries = new ArrayList<PeerState>();
+			entries.addAll(csv.readFile(f.getName()));
+			results.add(csv.metricsCalculator(entries));
+		}
+		
+		double p = ((double)csv.getConsumingTimeFed())/((double)csv.getTime());
+		double o = ((double)csv.getoFed())/((double)csv.getSupplyingTime());
+		double d = ((double)csv.getdTot())/((double)csv.getConsumingTimeTot());
+		double k = (p*d)/((1-p)*o);
+		
+		System.out.println("avg(P)="+p);
+		System.out.println("avg(O)="+o);
+		System.out.println("avg(D)="+d);
+		System.out.println("expected(avg(K))="+k);
+		
+		double fairness = -1, satisfaction = -1;
+		if(csv.getsFed()>0)
+			 fairness = ((double)csv.getrFed()/(double)csv.getsFed());		
+		if(csv.getdFed()>0)
+			satisfaction = ((double)csv.getrFed()/(double)csv.getdFed());
+		
+		results.add(new Results("global", fairness, satisfaction));
+		
+		
+		System.out.println("\n");
+		for(Results r : results)
+			System.out.println(r);
+	}
 
 }
